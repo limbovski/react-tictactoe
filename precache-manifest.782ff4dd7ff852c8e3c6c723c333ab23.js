5875033cabf1a3d53229ef868b166a976299b5f3
limbovski <limbovski@users.noreply.github.com>
2020-05-16 12:37:03 +0000

    Deploying to gh-pages from  @ b617f11436c351e20dce03937d05ede4dae59bfc 🚀

---
+++ precache-manifest.782ff4dd7ff852c8e3c6c723c333ab23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e5d0ad293773218929c90439e0e484",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "3ce6e4eacaedcd499d1e",
+    "url": "/react-tictactoe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c6d1a212f912059fc59f",
+    "url": "/react-tictactoe/static/js/2.a8f3d056.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tictactoe/static/js/2.a8f3d056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ce6e4eacaedcd499d1e",
+    "url": "/react-tictactoe/static/js/main.e19df38d.chunk.js"
+  },
+  {
+    "revision": "5dd1ffb7860dc425828f",
+    "url": "/react-tictactoe/static/js/runtime-main.6e853b57.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tictactoe/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f72d24a7c9084bd4ad8c2b3a97dc87b4.js"
+  "/react-tictactoe/precache-manifest.782ff4dd7ff852c8e3c6c723c333ab23.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tictactoe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
