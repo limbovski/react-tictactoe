e28a678e6790f32ef3b04d3ff3ee184e3f4ac33c
limbovski <limbovski@users.noreply.github.com>
2020-05-16 12:18:30 +0000

    Deploying to gh-pages from  @ 4f85709624f0f8017174db316662901866313605 🚀

---
+++ precache-manifest.f72d24a7c9084bd4ad8c2b3a97dc87b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caa8e1d88568de1fe4f2b20a349ca3ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "103cb76871456a11da82",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c6d1a212f912059fc59f",
+    "url": "/static/js/2.a8f3d056.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a8f3d056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "103cb76871456a11da82",
+    "url": "/static/js/main.7cc323dc.chunk.js"
+  },
+  {
+    "revision": "e436760d2a5e82b8f8dd",
+    "url": "/static/js/runtime-main.46f721ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
